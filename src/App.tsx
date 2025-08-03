016c1121ff9a5fda98d8bf3b7c7f7655d27786fe
Samarinara <samkatevatis@gmail.com>
2025-08-02 17:08:10 -0700

    rust test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import "nes.css/css/nes.min.css";
@@ -17,12 +16,15 @@ function App() {
     <div className="container">
       <div className="nes-container with-title is-centered m-4 p-6">
         <h2 className="title">Welcome To Pokemon Arena</h2>
-        <div id="center-contents">
+        <div id="center-contents"> 
           <div className="nes-field">
             <label htmlFor="name_field">Please enter your email to verify or create your account</label>
-            <input type="text" id="name_field" className="nes-input"></input>
+            <input type="text" id="name_field" className="nes-input" value={name} onChange={(e) => setName(e.target.value)} placeholder="Email"></input>
           </div>
-          <button type="button" className="nes-btn is-success">Success</button>
+
+          <button type="button" className="nes-btn is-success" onClick={greet}>Success</button>
+
+          <p>{greetMsg}</p>
         </div>
       </div>
     </div>
